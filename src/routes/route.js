5cdcebad54ba2ce8c8f57b24a790ed91041044b8
surajsingh419 <sursingh1996@gmail.com>
2021-12-18 08:19:34 +0530

    first commit

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,19 @@
 const express = require('express');
-
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
-});
+const CollegeController =require("../controllers/Collegecontroller")
+
+
+
+
+
+
+router.post('/functionup/colleges', CollegeController.registercollege)
+router.post('/functionup/intern',CollegeController.createintern );
+router.get('/functionup/interndetails',CollegeController. alldetails);
+
+
+
+
 
 module.exports = router;
